40f7eb3dcdf2023229769f7368497e81b8cefb2e
Ian-Meadows <iame5815@colorado.edu>
2021-02-07 15:33:11 -0700

    Added Player movement
    add some extra Scenes
    started some code

---
+++ Cool Game/Assets/Scripts/Beings/Being.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Being : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Init();
+    }
+
+    protected abstract void Init();
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    //Run in fixed update for MoveTo and RotateTo to function properly
+    protected abstract void Move();
+
+
+    //Call this within Move
+    protected void MoveTo(Vector2 point)
+    {
+        rb.MovePosition(point);
+    }
+
+    //Call this within Move
+    protected void RotateTo(float angle)
+    {
+        rb.MoveRotation(angle);
+    }
+}
---
+++ Cool Game/Assets/Scripts/Beings/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Being
+{
+
+    float speed = 10;
+
+    protected override void Init()
+    {
+        
+    }
+
+    protected override void Move()
+    {
+        float actualSpeed = speed * Time.deltaTime;
+
+        Vector3 dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir += Vector3.up * actualSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir += Vector3.down * actualSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir += Vector3.left * actualSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir += Vector3.right * actualSpeed;
+        }
+
+        //TODO: Add knockback?
+        //TODO: fix gliding
+        if(dir != Vector3.zero)
+        {
+            MoveTo(dir + transform.position);
+        }
+
+    }
+
+}
